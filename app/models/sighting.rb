63afee2b8463745f2fd15c46acb85cd3d18661df
JulieChristine <julie.wolsak@gmail.com>
2017-04-27 12:19:58 +0800

    add sighting to animal including CRUD functionalities

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,6 @@
 class Sighting < ActiveRecord::Base
+  belongs_to :animal
+
   validates :location, :presence => true
   validates :date, :presence => true
   validates :longitude, :presence => true
